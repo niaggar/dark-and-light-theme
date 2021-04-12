285a08a5cdb9c0c6a5610451809ea9bb8df73834
Nicolas Aguilera García <niagar25@gmail.com>
2021-04-11 19:54:54 -0500

    Local storage added

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const button = document.getElementById('button');
 const background = document.getElementById('container');
+const footer = document.getElementById('footer');
+const text = document.getElementById('txt-theme');
 
-button.addEventListener('click', () => {
+let theme;
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (localStorage.getItem('theme') == 'dark') {
+    changeTheme();
+  }
+});
+
+button.addEventListener('click', changeTheme);
+
+function changeTheme() {
   // Change the button activate or deactivate
   button.classList.toggle('active');
   button.classList.toggle('deactive');
@@ -9,11 +21,18 @@ button.addEventListener('click', () => {
   // Change the background acord to the button
   background.classList.toggle('light');
   background.classList.toggle('dark');
+  footer.classList.toggle('dark');
 
   // Determine the text to show on the button
   if (button.classList.contains('active')) {
+    theme = 'dark';
+    localStorage.setItem('theme', theme);
     button.innerHTML = 'Click me again';
+    text.innerHTML = 'Dark';
   } else {
+    theme = 'light';
+    localStorage.setItem('theme', theme);
     button.innerHTML = 'Click me';
+    text.innerHTML = 'Light';
   }
-});
+}
